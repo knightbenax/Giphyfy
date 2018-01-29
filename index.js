7fdae1492b912c698eefbdb8489e41f9ed64ea2e
Bezaleel Ashefor <knightbenax@gmail.com>
2018-01-29 13:50:14 +0100

    Move
    Version 0.1.0 is ready.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //Initialize all the things
 var delay;
+var image_results = [];
+const api_key = '3QIjIWWudRjqoTj65jOD3lThIpTA9Sx2';
+const url = 'http://api.giphy.com/v1/gifs/search?api_key=' + api_key + '&';
 
 //for snoppy brethrens
 function postJobs(){
@@ -15,15 +18,56 @@ function makeDelay(ms) {
     };
 };
 
+function createNode(element) {
+  return document.createElement(element); // Create the type of element you pass in the parameters
+}
+
+function append(parent, el) {
+  return parent.appendChild(el); // Append the second parameter(element) to the first one
+}
+
+function displayResults() {
+  var image_results_holder = document.getElementById('image_results');
+
+  //we want to remove previous results before we display the new one
+  image_results_holder.clearChildren;
+
+  //alert(image_results.length);
+
+  for (var i = 0; i < image_results.length; i++){
+
+    var image = createNode('div');
+    image.className = 'image_results__single';
+
+    var image_url = image_results[i]['images']['original']['url'];
+
+    image.style.backgroundImage = 'url(' + image_url + ')';
+
+    append(image_results_holder, image);
+  }
+
+  NProgress.done();
+}
+
 function fetchResult(){
+  NProgress.start();
   //the user's request
   var search = document.getElementById('search_input').value;
 
   //ping Giphy's API
-  fetch('http://api.giphy.com/v1/gifs/translate?api_key=3QIjIWWudRjqoTj65jOD3lThIpTA9Sx2&s=' + search).then(function (result) {
-    console.log('Response from the endpoint ' + result);
+  fetch( url + 'q=' + search + '&limit=20')
+    .then((resp) => resp.json())
+    .then(function (result) {
+      console.log(result);
+      image_results = result.data;
+      console.log(image_results);
+
+      displayResults();
   }, function (error){
-    console.log('An error occurred: ' + error);
+    NProgress.done();
+    //we would of course change this alert to a nice looking alert dialog
+    alert("Something went wrong. Issue has been logged and ninja monkeys dispatched. Please try again")
+    //console.log('An error occurred: ' + error);
   })
 }
 
